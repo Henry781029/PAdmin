8f44493c9a0e30617ea3dd90c0b8d1644fc2b03e
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-06-16 17:51:22 -0500

    cambio navbar

--- encabezados/heater.php
+++ encabezados/heater.php
@@ -36,7 +36,7 @@
                         <a class="nav-link" href="#">Configuracion</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">noticias</a>
+                        <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Informes</a>
                         </li>
                     </ul>
                     <br>
